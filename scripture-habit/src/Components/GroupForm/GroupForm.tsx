bc7ec1e4bed32923bbe5e50c18a9bf9db92f5039
Greice Moreira <greice.f.d.a@hotmail.com>
2025-11-13 13:33:04 -0500

    Simple structure

---
+++ scripture-habit/src/Components/GroupForm/GroupForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GroupForm.css';
+
+const GroupForm: React.FC = () => {
+  return (
+    <div className="GroupForm">
+      <p>Group Form</p>
+    </div>
+  );
+};
+
+export default GroupForm;
---
+++ scripture-habit/src/Components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero: React.FC = () => {
+  return (
+    <div className="Hero">
+      <p>Hero</p>
+    </div>
+  );
+};
+
+export default Hero;
